25e0866a390472d42053567cb2fdee52599f0457
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-08-04 17:49:18 -0500

    week2

---
+++ week2/appending-elements/index.js
@@ -0,0 +1,31 @@
+var newH1 = document.createElement("h1")
+newH1.textContent = "Welcome to my js site!"
+document.body.append(newH1)
+
+
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript"
+document.body.append(newP)
+
+var newOl = document.createElement("ol")
+document.body.append(newOl)
+
+var newli1 = document.createElement("li")
+newli1.textContent = "one"
+newOl.append(newli1)
+
+var newLi2 = document.createElement("li")
+newLi2.textContent = "two"
+newOl.append(newLi2)
+
+var newLi3 = document.createElement("li")
+newLi3.textContent = "three"
+newOl.append(newLi3)
+
+
+
+
+
+
+
+
